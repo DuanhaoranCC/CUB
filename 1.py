1f24847522f5a63a2811a573c1530f0f1208acea
dhr1997 <66892515+DuanhaoranCC@users.noreply.github.com>
2022-03-20 19:04:41 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from torch_geometric.data import HeteroData
 import torch_geometric.transforms as T
+from torch_geometric.datasets import OGB_MAG
 from torch_geometric.nn import SAGEConv, to_hetero
 
 path = "./new/"
@@ -18,9 +19,11 @@ relation_feat = {}
 A_feat = []
 B_feat = []
 metapath_dict = {}
-
+# 389
 with open('./color.txt', "r") as f:
-    for index, color in enumerate(set(f.read().split('\n'))):
+    o = set(f.read().split('\n'))
+    o.remove('')
+    for index, color in enumerate(o):
         class_B[color.strip()] = index
 
 with open('./keyword.txt', "r") as f:
@@ -42,8 +45,9 @@ for index, i in enumerate(os.listdir(path)):
             relation[src.strip()].append([index, class_B[dst.strip()]])
 
 data['A'].x = torch.rand(200, 768)
-data['B'].x = torch.rand(328, 768)
-for i in relation:
+data['B'].x = torch.rand(389, 768)
+
+for index, i in enumerate(relation):
     data['A', i, 'B'].edge_index = torch.tensor(np.array(relation[i]), dtype=torch.long).t().contiguous().view(2, -1)
     data['B', i, 'A'].edge_index = data['A', i, 'B'].edge_index.flip([0])
     # data['A', i, 'B'].edge_attr = torch.rand(np.array(relation[i]).shape[0], 768)
@@ -61,7 +65,7 @@ class GNN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return x
 
+
 model = GNN(hidden_channels=64, out_channels=16)
 model = to_hetero(model, data.metadata(), aggr='sum')
 out = model(data.x_dict, data.edge_index_dict)
-print(out)
