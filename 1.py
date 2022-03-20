7ada1e4b918e841f7a594689b2bc4e8609e2f1c4
dhr1997 <66892515+DuanhaoranCC@users.noreply.github.com>
2022-03-20 11:45:38 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,65 +1,54 @@
 from string import digits
 import os
 import torch
-import torch.optim as optim
 import pandas as pd
 import numpy as np
 from torch_geometric.data import HeteroData
 import torch_geometric.transforms as T
-from torch_geometric.datasets import OGB_MAG
 from torch_geometric.nn import SAGEConv, to_hetero
 
-# example:
-# dataset = OGB_MAG(root='./data', preprocess='metapath2vec')
-# data = dataset[0]
-# paper:736389
-# author:1134649
-# cites_edge_data = data['paper', 'cites', 'paper'].edge_index
-# writes_edge = data['author', 'writes', 'paper'].edge_index
+path = ".//new/"
 
-path = "./CUB/"
 data = HeteroData()
+
 class_A = {}
+class_B = {}
+relation = {}
+relation_feat = {}
 A_feat = []
 B_feat = []
 metapath_dict = {}
 
-B = pd.read_excel(os.path.join(path, 'color.xlsx'), sheet_name="df")
-class_B = set([j.replace('-', ' ') for index, j in enumerate(B['color'])])
-class_B = {color: index for index, color in enumerate(class_B)}
+with open('./color.txt', "r") as f:
+    for index, color in enumerate(set(f.read().split('\n'))):
+        class_B[color.strip()] = index
 
-edge = pd.read_excel(os.path.join(path, 'keyword.xlsx'), sheet_name="df")
-relation = {i: [] for i in edge['keyword']}
-relation2 = {i: [] for i in edge['keyword']}
-relation_feat = {i: [] for i in edge['keyword']}
+with open('./keyword.txt', "r") as f:
+    for _, keyword in enumerate(set(f.read().split('\n'))):
+        if len(keyword.strip()) > 1:
+            relation[keyword.strip()] = []
+            relation_feat[keyword.strip()] = []
 
-for index, i in enumerate(os.listdir(os.path.join(path, "extract"))):
+for index, i in enumerate(os.listdir(path)):
     table = str.maketrans('', '', digits)
     class_label = i.translate(table).split('.')[-2].replace('_', ' ')
     class_A[class_label] = index
 
-    with open(os.path.join(path, "extract", i), "r") as f:
+    with open(os.path.join(path, i), "r") as f:
         txt = f.read()
     for j in txt.split('\n'):
         if len(j.split(',')) == 2:
-            # src:edge, class_label and dst is node pair.
             src, dst = j.split(',')
+            relation[src.strip()].append([index, class_B[dst.strip()]])
 
-            relation[src.strip()].append([class_A[class_label], class_B[dst.strip().replace('-', ' ')]])
-            # reverse edge
-            relation2[src.strip()].append([class_B[dst.strip().replace('-', ' ')], class_A[class_label]])
-
-
-data['A'].x = torch.rand(200,768)
-data['B'].x = torch.rand(430,768)
+data['A'].x = torch.rand(200, 768)
+data['B'].x = torch.rand(328, 768)
 for i in relation:
-    data['A', i, 'B'].edge_index = torch.tensor(np.array(relation[i]), dtype=torch.long).t().contiguous()
-    data['B', i, 'A'].edge_index = torch.tensor(np.array(relation2[i]), dtype=torch.long).t().contiguous()
-    data['A', i, 'B'].edge_attr = torch.rand(np.array(relation[i])[1], 768)
-    data['B', i, 'A'].edge_attr = torch.rand(np.array(relation[i])[1], 768)
+    data['A', i, 'B'].edge_index = torch.tensor(np.array(relation[i]), dtype=torch.long).t().contiguous().view(2, -1)
+    data['B', i, 'A'].edge_index = data['A', i, 'B'].edge_index.flip([0])
+    # data['A', i, 'B'].edge_attr = torch.rand(np.array(relation[i]).shape[0], 768)
+    # data['B', i, 'A'].edge_attr = torch.rand(np.array(relation[i]).shape[0], 768)
 
-
-print(data)
 
 class GNN(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels):
@@ -71,7 +60,6 @@ class GNN(torch.nn.Module):
         x = self.conv1(x, edge_index).relu()
         x = self.conv2(x, edge_index)
         return x
-
 
 model = GNN(hidden_channels=64, out_channels=16)
 model = to_hetero(model, data.metadata(), aggr='sum')
